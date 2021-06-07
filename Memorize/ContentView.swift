5a46bc92ae923d8230b2bd5b35076414c5eacfae
GuidoMr <guidomellorezende@gmail.com>
2021-06-07 12:49:39 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Guido Rezende on 07/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Guido Rezende on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
